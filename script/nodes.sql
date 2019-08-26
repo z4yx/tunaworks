221d41f4a1588a5dc69a6cb40a40bf58a476ec1b
z4yx <z4yx@users.noreply.github.com>
2019-08-26 10:21:18 +0800

    update schema

--- script/nodes.sql
+++ script/nodes.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS `nodes`;
 CREATE TABLE `nodes`(
     `node`      INTEGER PRIMARY KEY ASC,
     `name`      VARCHAR NOT NULL,
+    `proto`     INT NOT NULL,
+    `token`     VARCHAR NOT NULL,
     `active`    BOOLEAN NOT NULL
 );
 
