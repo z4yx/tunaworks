be65aa8a17dbd1de98eb8756b95f87a5f4ea12d5
z4yx <z4yx@users.noreply.github.com>
2019-08-27 14:10:29 +0800

    add config file support for prober

--- cli/prober_main.go
+++ cli/prober_main.go
@@ -1,16 +1,65 @@
 package main
 
-import prober "github.com/z4yx/tunaworks/prober"
+import (
+	"errors"
+	"os"
+	"fmt"
 
-func main() {
-	cfg := prober.ProberConfig{
-		Debug:    true,
-		IPv4:     true,
-		IPv6:     true,
-		Server:   "192.168.1.50",
-		Token:    "041c9bb6-5fbe-45a8-98da-1d2951e4f862",
-		Interval: 60,
+	toml "github.com/BurntSushi/toml"
+	prober "github.com/z4yx/tunaworks/prober"
+	"gopkg.in/op/go-logging.v1"
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+var logger = logging.MustGetLogger("tunaworks")
+
+func loadConfig(cfgFile string, cfg *prober.ProberConfig) error {
+	if cfgFile != "" {
+		if _, err := toml.DecodeFile(cfgFile, cfg); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func parseSettings(c *cli.Context, cfg *prober.ProberConfig) error {
+	if c.Bool("help") {
+		cli.ShowAppHelpAndExit(c, 0)
+	}
+	cf := c.GlobalString("config-file")
+	if len(cf) == 0 {
+		return errors.New("Config file not specified")
+	}
+	return loadConfig(cf, cfg)
+}
+
+func cmdRun(c *cli.Context) error {
+	cfg := prober.ProberConfig{}
+	err := parseSettings(c, &cfg)
+	if err != nil {
+		fmt.Println(err)
+		return err
 	}
 	s := prober.MakeProber(&cfg)
 	s.Run()
+	return nil
+}
+
+func main() {
+	app := &cli.App{
+		Name:      "tunaworks-prober",
+		UsageText: `tunaworks-prober [options]`,
+		Usage:     "TUNA.works prober",
+		Version:   "1.0",
+		Flags: []cli.Flag{
+			&cli.StringFlag{Name: "config-file, c", Usage: "`path` to your config file"},
+		},
+		Action: cmdRun,
+		Authors: []cli.Author{
+			{Name: "Yuxiang Zhang", Email: "yuxiang.zhang@tuna.tsinghua.edu.cn"},
+		},
+	}
+
+	app.Run(os.Args)
 }
