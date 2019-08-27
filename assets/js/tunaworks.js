7dda5b8bf3db02c651cde5ccbf66b13b3981cd3a
z4yx <z4yx@users.noreply.github.com>
2019-08-27 12:06:09 +0800

    update frontenc

--- assets/js/tunaworks.js
+++ assets/js/tunaworks.js
@@ -1,6 +1,5 @@
 function CertIsAboutToExpire(dateString) {
     let epoch = (new Date(dateString)).getTime();
-    if (epoch === 0) return false;
     return epoch - Date.now() < 7 * 86400 * 1000;
 }
 Vue.component('top-nav-bar', {
@@ -28,55 +27,86 @@ Vue.component('top-nav-bar', {
 var overall = document.getElementById('overall') && new Vue({
     el: '#overall',
     data: {
-        latestInfo: {
-            NodeNames: {},
-            Websites: [],
-        }
+        secondRow: [],
+        nodeNames: [],
+        websites: [],
     },
     methods: {
-        insertDispProp: function (info) {
-            info.Websites.forEach((site) => {
-                for (let node in site.Nodes) {
-                    let rec = site.Nodes[node];
-                    rec.ClassObj = {
-                        "cell-site-state": true
-                    };
-                    rec.Icon = {
-                        "fas": true
-                    };
-                    if (rec.SSLError !== null || rec.StatusCode >= 400) {
-                        rec.ClassObj["alert-danger"] = true;
-                        rec.Icon["fa-times"] = true;
-                        if (rec.SSLError !== null) {
-                            rec.Line1 = "SSL";
+        buildCellContent: function(info, site, protocol) {
+            let nodes = protocol == 4 ? site.Nodes4 : site.Nodes6;
+            for (let node_id in nodes) {
+                let rec = nodes[node_id];
+                rec.ClassObj = {
+                    "cell-site-state": true
+                };
+                rec.Icon = {
+                    "fas": true
+                };
+                if (rec.SSLError !== null || rec.StatusCode >= 400) {
+                    rec.ClassObj["alert-danger"] = true;
+                    rec.Icon["fa-times"] = true;
+                    if (rec.SSLError !== null) {
+                        rec.Line1 = "ERROR";
+                        if(rec.SSLError.length > 20){
+                            let suffix = rec.SSLError.slice(-20);
+                            if(suffix.indexOf(' ') > 0)
+                                suffix = suffix.slice(suffix.indexOf(' '));
+                            rec.Line2 = '...' + suffix;
+                            rec.Details = rec.SSLError;
+                        }else
                             rec.Line2 = rec.SSLError;
-                        } else {
-                            rec.Line1 = rec.StatusCode;
-                            rec.Line2 = rec.ResponseTime + " ms";
-                        }
-                    } else if (CertIsAboutToExpire(rec.SSLExpire)) {
-                        rec.ClassObj["alert-warning"] = true;
-                        rec.Icon["fa-exclamation-triangle"] = true;
-                        rec.Line1 = rec.StatusCode;
-                        rec.Line2 = 'Cert will expire on ' + (new Date(rec.SSLExpire)).toLocaleDateString();
                     } else {
-                        rec.ClassObj["alert-success"] = true;
-                        rec.Icon["fa-check"] = true;
                         rec.Line1 = rec.StatusCode;
                         rec.Line2 = rec.ResponseTime + " ms";
                     }
-                    console.debug(rec);
+                } else if (site.Url.startsWith("https:") && CertIsAboutToExpire(rec.SSLExpire)) {
+                    rec.ClassObj["alert-warning"] = true;
+                    rec.Icon["fa-exclamation-triangle"] = true;
+                    rec.Line1 = rec.StatusCode;
+                    rec.Line2 = 'Cert will expire on ' + (new Date(rec.SSLExpire)).toLocaleDateString();
+                } else {
+                    rec.ClassObj["alert-success"] = true;
+                    rec.Icon["fa-check"] = true;
+                    rec.Line1 = rec.StatusCode;
+                    rec.Line2 = rec.ResponseTime + " ms";
                 }
-                for (let node in info.NodeNames)
-                    if (!(node in site.Nodes))
-                        site.Nodes[node] = {};
+                // console.debug(rec);
+            }
+            for (let node_id in info.NodeNames)
+                if (!(node_id in nodes))
+                    nodes[node_id] = {};
+        },
+        buildDispProp: function (info) {
+            let secondRow = [];
+            let nodeNames = [];
+            for (let node_id in info.NodeNames) {
+                secondRow.push("IPv4");
+                secondRow.push("IPv6");
+                nodeNames.push([node_id, info.NodeNames[node_id]]);
+            }
+            this.nodeNames = nodeNames;
+            this.secondRow = secondRow;
+            let websites = [];
+            info.Websites.forEach((site) => {
+                this.buildCellContent(info, site, 4);
+                this.buildCellContent(info, site, 6);
+                let records = [];
+                for (let pair of nodeNames) { // order matters
+                    records.push(site.Nodes4[pair[0]]);
+                    records.push(site.Nodes6[pair[0]]);
+                }
+                websites.push({
+                    Url: site.Url,
+                    Records: records,
+                });
             });
+
+            this.websites = websites;
         },
         loadLatest: function () {
             this.$http.get('monitor/latest').then((resp) => {
                 let body = resp.body;
-                this.insertDispProp(body);
-                this.latestInfo = body;
+                this.buildDispProp(body);
             }, () => {
 
             });
@@ -97,15 +127,35 @@ var ssl = document.getElementById('ssl') && new Vue({
             info.Websites.forEach((site) => {
                 let latestUpdate = 0;
                 let rec = null;
-                for (let node in site.Nodes) {
-                    let item = site.Nodes[node];
+                let nodes = [];
+                if(site.Url.startsWith("http:"))
+                    return;
+                for (let node in site.Nodes4)
+                    nodes.push(site.Nodes4[node]);
+                for (let node in site.Nodes6)
+                    nodes.push(site.Nodes6[node]);
+                for (let item of nodes) {
+                    if(rec === null) {
+                        rec = item;
+                        continue;
+                    }
+                    // We're interested in x509 errors only
+                    if(item.SSLError !== null && !item.SSLError.startsWith('x509'))
+                        continue;
+                    if(rec.SSLError !== null && item.SSLError === null){
+                        rec = item;
+                        continue;
+                    }
+                    if(rec.SSLError === null && item.SSLError !== null){
+                        continue;
+                    }
                     let updated = (new Date(item.Updated)).getTime();
                     if(updated > latestUpdate) {
                         latestUpdate = updated;
                         rec = item;
                     }
                 }
-                if(rec === null || (rec.SSLError === null && rec.SSLExpire.startsWith('1970')))
+                if(rec === null)
                     return;
                 let item = {
                     ClassObj: {},
