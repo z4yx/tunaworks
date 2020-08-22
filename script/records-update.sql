2cf64c9e845d08a6d4c7c7fd81673ba72d0f46ab
z4yx <z4yx@users.noreply.github.com>
2020-08-22 12:14:21 +0800

    fix the error on Mysql

--- script/records-update.sql
+++ script/records-update.sql
@@ -3,7 +3,7 @@ ALTER TABLE `records`
 ALTER TABLE `records`
     ADD COLUMN `ocsp_err`  VARCHAR(256) DEFAULT NULL;
 ALTER TABLE `records`
-    ADD COLUMN `ocsp_this_update` TIMESTAMP DEFAULT NULL;
+    ADD COLUMN `ocsp_this_update` TIMESTAMP DEFAULT 0;
 ALTER TABLE `records`
-    ADD COLUMN `ocsp_next_update` TIMESTAMP DEFAULT NULL;
+    ADD COLUMN `ocsp_next_update` TIMESTAMP DEFAULT 0;
 
